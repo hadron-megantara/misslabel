ea8382d8ce781e727e996891be9e7fce14403a50
Hadron Megantara <hadronmegantara@Hadrons-MacBook-Pro.local>
2017-11-06 17:22:41 +0700

    add materialTransactions

---
+++ database/migrations/2017_11_06_095708_create_material_transactions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMaterialTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('material_transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_material');
+            $table->string('description');
+            $table->integer('price');
+            $table->date('date_purchase');
+            $table->string('name');
+            $table->unsignedInteger('size');
+            $table->string('mime');
+            $table->timestamps();
+        });
+
+        DB::statement('ALTER TABLE material_transactions ADD file LONGBLOB');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('material_transactions');
+    }
+}
