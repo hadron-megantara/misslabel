240d56e81804a37f8cb8dff81c897b9795215016
Hadron Megantara <hadronmegantara@Hadrons-MacBook-Pro.local>
2017-11-29 17:06:13 +0700

    Warehouse-Store-Stock

---
+++ database/migrations/2017_11_29_095851_create_store_stocks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStoreStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('store_stocks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_id');
+            $table->integer('warehouse_store_id');
+            $table->integer('store_id');
+            $table->integer('total_product');
+            $table->char('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('store_stocks');
+    }
+}
